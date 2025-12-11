e59bc29a96e7fbfff1ae429f25776ecf571af4e5
hjduncan <126494580+hjduncan@users.noreply.github.com>
2025-12-11 22:18:27 +0000

    update base page validation check

--- pageObjects/BasePage.ts
+++ pageObjects/BasePage.ts
@@ -10,22 +10,20 @@ export class BasePage {
     }
 
     protected async validateElements(elementsToCheck: { locator: Locator; count: number; expectedText?: string }[]) {
-        // @ts-ignore
-        const domObjects = elementsToCheck.map((element) => element.locator['_selector']);
-        // Wait for all elements to be present in the DOM
-        for (const selector of domObjects) {
-            await this.page.waitForSelector(selector);
-        }
+        // Wait for all elements to be attached to the DOM concurrently
+        await Promise.all(elementsToCheck.map(e => e.locator.first().waitFor()));
+
         for (const { locator, count, expectedText } of elementsToCheck) {
             // Validate count
             const actualCount = await locator.count();
-            // Assert the count matches the expected count
             expect(actualCount).toEqual(count);
 
-            // Handle different validation methods dynamically
+            // Validate text if provided
             if (expectedText) {
-                const actualText = await locator.textContent();
-                expect(actualText?.trim()).toContain(expectedText?.trim());
+                const texts = await locator.allTextContents();
+                // Check that at least one element contains the expected text
+                const hasExpectedText = texts.some(text => text.trim().includes(expectedText.trim()));
+                expect(hasExpectedText).toBe(true);
             }
         }
     }
